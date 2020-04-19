b2c9e5854bf9beb9971d03b28682e3358492f409
lipd <lipeidao@gmail.com>
2020-04-19 18:13:26 +0800

    fix bug of grid

--- src/components/grid.js
+++ src/components/grid.js
@@ -3,24 +3,25 @@ import styled from '@emotion/styled'
 export const Row = styled('section')`
   display: flex;
   flex-flow: row wrap;
-  justify-content: space-between;
 `
 
 export const Col = styled('div')`
-  margin-bottom: 20px;
+  margin-bottom: 1.5rem;
   flex: 0 0 100%;
 `
 
 export const Col3 = styled(Col)`
+  padding: 10px;
   @media (min-width: 575px) {
     flex: 0 0 48%;
   }
   @media (min-width: 768px) {
-    flex: 0 0 32%;
+    flex: 0 0 33.33%;
   }
 `
 
 export const Col4 = styled(Col)`
+  padding: 10px;
   @media (min-width: 575px) {
     flex: 0 0 32%;
   }
